3e11ed9005dd8527785dabc1aff70b1882698b00
Dave McFarland <dave@sawmac.com>
2014-07-19 16:28:51 -0700

    Add chapter 10 tutorial files

---
+++ chapter10/airports.js
@@ -0,0 +1,17 @@
+var airports = [
+  { value : "PDX", label: "Portland International Airport, Portland, OR" },
+  { value : "SFO", label: "San Francisco, CA" },
+  { value : "SJC", label: "San Jose, CA" },
+  { value : "KOA", label: "Keahole, HI" },
+  { value : "ORD", label: "Chicago O'Hare International Airport, Chicago, IL" },
+  { value : "MDW", label: "Chicago Midway Airport, Chicago, IL" },
+  { value : "JFK", label: "John F. Kennedy International Airport, New York, NY" },
+  { value : "LGA", label: "La Guardia Airport, New York, NY" },
+  { value : "IAD", label: "Washington Dulles International Airport, Washington, DC" },
+  { value : "CDG", label: "Charles De Gaulle International Airport, Paris, France" },
+  { value : "CGH", label: "Congonhas International Airport, Sao Paulo, Brazil" },
+  { value : "CGK", label: "Soekarno-Hatta International Airport, Jakarta, Indonesia" },
+  { value : "LHR", label: "London Heathrow Airport, London, United Kingdom" },
+  { value : "FRA", label: "Frankfurt International Airport, Frankfurt, Germany" },
+  { value : "HND", label: "Tokyo International Airport, Tokyo, Japan" },
+];
